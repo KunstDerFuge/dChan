04b200f81fbfac34dbfafacdf7e937097dcfbae2
Robert Amour <robert@ferretech.io>
2021-07-26 02:21:03 -0700

    post_id is not unique; don't use as primary key

--- posts/models.py
+++ posts/models.py
@@ -5,10 +5,15 @@ class Post(models.Model):
     platform = models.CharField(max_length=12)
     board = models.CharField(max_length=60)
     thread_id = models.IntegerField()
-    post_id = models.IntegerField(primary_key=True)
+    post_id = models.IntegerField()
     author = models.CharField(max_length=180)
     subject = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
     tripcode = models.CharField(max_length=30, default=None)
     is_op = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['platform', 'board', 'post_id'], name='unique_post')
+        ]
