d3b1465b52f686b5f3c5f28a69120ed346bb3402
Robert Amour <robert@ferretech.io>
2021-07-27 02:30:08 -0700

    Update URL schema, add board and platform index

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /posts/
     path('', views.index, name='index'),
-    # ex: /posts/5/
-    path('<int:thread_id>/', views.thread, name='thread'),
+    path('<str:platform>/', views.index, name='index'),
+    path('<str:platform>/<str:board>/', views.index, name='index'),
+    path('<str:platform>/<str:board>/res/<int:thread_id>.html', views.thread, name='thread'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,13 @@ from django.template import loader
 from posts.models import Post
 
 
-def index(request):
-    thread_list = Post.objects.filter(is_op=True).order_by('-timestamp')[:50]
+def index(request, platform=None, board=None):
+    if board:
+        thread_list = Post.objects.filter(is_op=True, platform=platform, board=board).order_by('-timestamp')[:50]
+    elif platform:
+        thread_list = Post.objects.filter(is_op=True, platform=platform).order_by('-timestamp')[:50]
+    else:
+        thread_list = Post.objects.filter(is_op=True).order_by('-timestamp')[:50]
     template = loader.get_template('posts/index.html')
     context = {
         'thread_list': thread_list,
@@ -12,8 +17,8 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
-def thread(request, thread_id):
-    thread_posts = Post.objects.filter(thread_id=thread_id).order_by('timestamp')
+def thread(request, platform, board, thread_id):
+    thread_posts = Post.objects.filter(platform=platform, board=board, thread_id=thread_id).order_by('post_id')
     template = loader.get_template('posts/posts.html')
     context = {
         'posts': thread_posts,
