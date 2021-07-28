22845b9b8f5546a48ba0939ca1757832a451f8ad
Robert Amour <robert@ferretech.io>
2021-07-28 15:13:34 -0700

    Parse links from HTML in load_chan_data command

--- posts/management/commands/load_chan_data.py
+++ posts/management/commands/load_chan_data.py
@@ -1,6 +1,9 @@
 from csv import DictReader
 from django.core.management import BaseCommand
 from tqdm import tqdm
+import pandas as pd
+import re
+import html as html_
 
 from posts.models import Post
 
@@ -48,6 +51,22 @@ def parse_formatting(html):
     return final_text
 
 
+def process_links(text):
+    links = dict()
+    matches = re.findall(
+        r'\"\/([a-zA-Z0-9]+)\/res\/([0-9]+).html#q?([0-9]+)\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/[0-9]+)', text)
+    if len(matches) == 0:
+        return dict()
+    else:
+        for match in matches:
+            if match[1] == match[2]:
+                # Special logic for OP post URLs
+                links[html_.unescape(match[-1])] = f"/{match[0]}/res/{match[1]}.html"
+            else:
+                links[html_.unescape(match[-1])] = f"/{match[0]}/res/{match[1]}.html#{match[2]}"
+    return links
+
+
 def split_list(lst, n):
     from itertools import islice
     lst = iter(lst)
@@ -64,17 +83,27 @@ class Command(BaseCommand):
             if delete == 'y':
                 Post.objects.all().delete()
 
+        tqdm.pandas()
+
         for platform in ['4chan', '8chan', '8kun']:
             print(f'Loading {platform} data...')
             try:
-                reader = DictReader(open(f'{platform}.csv'))
-                row_count = sum(1 for row in reader)
+                df = pd.read_csv(f'{platform}.csv')
+
+                print('Processing links...')
+                df['links'] = df.body_text.progress_apply(process_links)
+
+                print('Parsing HTML to imageboard markup...')
+                df['body_text'] = df.body_text.progress_apply(parse_formatting)
+
+                print('Committing objects to database...')
                 new_posts = []
-                for row in tqdm(DictReader(open(f'{platform}.csv')), total=row_count):
+                for index, row in tqdm(df.iterrows(), total=len(df)):
                     post = Post(platform=platform, board=row['board'], thread_id=row['thread_no'],
-                                post_id=row['post_no'], author=row['name'], subject=row['subject'],
-                                body=row['body_text'], timestamp=row['timestamp'], tripcode=row['tripcode'],
-                                is_op=(row['post_no'] == row['thread_no']))
+                                post_id=row['post_no'], author=row['name'], poster_hash=row['poster_id'],
+                                subject=row['subject'], body=row['body_text'], timestamp=row['timestamp'],
+                                tripcode=row['tripcode'], is_op=(row['post_no'] == row['thread_no']),
+                                links=row['links'])
                     new_posts.append(post)
                     if len(new_posts) >= 10000:
                         Post.objects.bulk_create(new_posts)
