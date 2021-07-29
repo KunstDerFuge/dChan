653b00de529727efe8493b953f9e9075ec6df387
Robert Amour <robert@ferretech.io>
2021-07-28 20:45:44 -0700

    If not archived, provide archive.is link

--- posts/templatetags/filters.py
+++ posts/templatetags/filters.py
@@ -12,3 +12,13 @@ register = template.Library()
 @stringfilter
 def markdown(text, links):
     return mark_safe(md.markdown(text, extensions=['nl2br', ChanExtensions(links)]))
+
+
+@register.filter(name='get_archive_link')
+@stringfilter
+def get_archive_link(path):
+    parts = path.split('/')[1:]
+    print(parts)
+    new_path = '/'.join(parts[1:])
+    site = {'8chan': 'https://8ch.net', '8kun': 'https://8kun.top'}
+    return f'https://archive.is/{site[parts[0]]}/{new_path}'
