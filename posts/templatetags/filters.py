00127c24f62298c5ed80eead2bca397861000976
Robert Amour <robert@ferretech.io>
2021-07-29 01:08:03 -0700

    Pastelize poster ID background colors

--- posts/templatetags/filters.py
+++ posts/templatetags/filters.py
@@ -23,13 +23,35 @@ def get_archive_link(path):
     return f'https://archive.is/{site[parts[0]]}/{new_path}'
 
 
+def hex_to_rgb(hex_):
+    r = int(hex_[:2], 16) / 255
+    g = int(hex_[2:4], 16) / 255
+    b = int(hex_[4:], 16) / 255
+    return r, g, b
+
+
+def rgb_to_hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(int(r*255), int(g*255), int(b*255))
+
+
 @register.filter(name='contrast_text')
 @stringfilter
 def contrast_text(bg_color):
-    r = int(bg_color[:2], 16) / 255
-    g = int(bg_color[2:4], 16) / 255
-    b = int(bg_color[4:], 16) / 255
+    if bg_color.startswith('#'):
+        bg_color = bg_color[1:]
+    r, g, b = hex_to_rgb(bg_color)
     luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
-    if luminance > 0.6:
+    if luminance > 0.65:
         return 'black'
     return 'white'
+
+
+@register.filter(name='pastelize')
+@stringfilter
+def pastelize(poster_hash):
+    r, g, b = hex_to_rgb(poster_hash)
+    r += (1 - r) / 2
+    g += (1 - g) / 2
+    b += (1 - b) / 2
+    return rgb_to_hex(r, g, b)
+
