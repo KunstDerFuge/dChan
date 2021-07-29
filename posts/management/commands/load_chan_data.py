c0d51c0d304bb5815531c38a34b628cac9a1d0e0
Robert Amour <robert@ferretech.io>
2021-07-28 20:05:58 -0700

    Update link processing, correctly handle board index links

--- posts/management/commands/load_chan_data.py
+++ posts/management/commands/load_chan_data.py
@@ -1,9 +1,9 @@
-from csv import DictReader
+import html as html_
+import re
+
+import pandas as pd
 from django.core.management import BaseCommand
 from tqdm import tqdm
-import pandas as pd
-import re
-import html as html_
 
 from posts.models import Post
 
@@ -51,19 +51,26 @@ def parse_formatting(html):
     return final_text
 
 
-def process_links(text):
+def process_links(row):
     links = dict()
+    board_index_links = re.findall(r'\"\/([a-zA-Z0-9]+)\/index\.html\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/)',
+                                   row['body_text'])
     matches = re.findall(
-        r'\"\/([a-zA-Z0-9]+)\/res\/([0-9]+).html#q?([0-9]+)\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/[0-9]+)', text)
+        r'\"\/([a-zA-Z0-9]+)\/res\/([0-9]+)\.html#q?([0-9]+)\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/[0-9]+)',
+        row['body_text'])
+    matches.extend(board_index_links)
     if len(matches) == 0:
         return dict()
     else:
         for match in matches:
-            if match[1] == match[2]:
+            if len(match) == 2:
+                # Board index link
+                links[html_.unescape(match[-1])] = f"/{match[0]}/"
+            elif match[1] == match[2]:
                 # Special logic for OP post URLs
-                links[html_.unescape(match[-1])] = f"/{match[0]}/res/{match[1]}.html"
+                links[html_.unescape(match[-1])] = f"{row['platform']}/{match[0]}/res/{match[1]}.html"
             else:
-                links[html_.unescape(match[-1])] = f"/{match[0]}/res/{match[1]}.html#{match[2]}"
+                links[html_.unescape(match[-1])] = f"{row['platform']}/{match[0]}/res/{match[1]}.html#{match[2]}"
     return links
 
 
@@ -93,9 +100,10 @@ class Command(BaseCommand):
                 for file in files:
                     print(f'Loading {file}...')
                     df = pd.read_csv(file)
+                    df['platform'] = platform
 
                     print('Processing links...')
-                    df['links'] = df.body_text.progress_apply(process_links)
+                    df['links'] = df.progress_apply(process_links, axis=1)
 
                     print('Parsing HTML to imageboard markup...')
                     df['body_text'] = df.body_text.progress_apply(parse_formatting)
@@ -104,13 +112,13 @@ class Command(BaseCommand):
                     print('Committing objects to database...')
                     new_posts = []
                     for index, row in tqdm(df.iterrows(), total=len(df)):
-                        post = Post(platform=platform, board=row['board'], thread_id=row['thread_no'],
+                        post = Post(platform=row['platform'], board=row['board'], thread_id=row['thread_no'],
                                     post_id=row['post_no'], author=row['name'], poster_hash=row['poster_id'],
                                     subject=row['subject'], body=row['body_text'], timestamp=row['timestamp'],
                                     tripcode=row['tripcode'], is_op=(row['post_no'] == row['thread_no']),
                                     links=row['links'])
                         new_posts.append(post)
-                        if len(new_posts) >= 5000:
+                        if len(new_posts) >= 10000:
                             Post.objects.bulk_create(new_posts)
                             new_posts = []
 
