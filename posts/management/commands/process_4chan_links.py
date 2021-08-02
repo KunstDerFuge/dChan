c1b409718e8898a1068032a0b8198fb16faca7f8
Robert Amour <robert@ferretech.io>
2021-08-02 14:39:29 -0700

    Move old process_links code to process_4chan_links

--- posts/management/commands/process_4chan_links.py
+++ posts/management/commands/process_4chan_links.py
@@ -10,6 +10,6 @@ class Command(BaseCommand):
     help = "Process >>links and store their full URLs for fast retrieval"
 
     def handle(self, *args, **options):
-        posts = Post.objects.all()
+        posts = Post.objects.filter(platform='4chan')
         for post in tqdm(posts):
             post.process_links()
