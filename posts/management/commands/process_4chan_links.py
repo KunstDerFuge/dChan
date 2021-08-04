aa1ca0eaf79a9a07b16fce789aaaec36f4d3e023
Robert Amour <robert@ferretech.io>
2021-08-04 15:38:33 -0700

    Update process_4chan_links.py

--- posts/management/commands/process_4chan_links.py
+++ posts/management/commands/process_4chan_links.py
@@ -3,13 +3,14 @@ from django.core.management import BaseCommand
 from tqdm import tqdm
 import re
 
-from posts.models import Post
+from posts.models import Post, Platform
 
 
 class Command(BaseCommand):
     help = "Process >>links and store their full URLs for fast retrieval"
 
     def handle(self, *args, **options):
-        posts = Post.objects.filter(platform='4chan')
+        fourch = Platform.objects.get(name='4chan')
+        posts = Post.objects.filter(platform=fourch)
         for post in tqdm(posts):
             post.process_links()
