4eaf5f0bce7a13b0c8e702f9e11cf5764c13620c
Robert Amour <robert@ferretech.io>
2021-08-04 20:20:42 -0700

    Cache pages by URL

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 from .views import SearchResultsView, AdvancedSearch
@@ -8,7 +9,7 @@ urlpatterns = [
     path('search/advanced/', AdvancedSearch.as_view(), name='advanced_search'),
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('drop/<int:drop_no>', views.drop, name='drop'),
-    path('<str:platform>/', views.index, name='index'),
-    path('<str:platform>/<str:board>/', views.index, name='index'),
-    path('<str:platform>/<str:board>/res/<int:thread_id>.html', views.thread, name='thread'),
+    path('<str:platform>/', cache_page(60 * 15)(views.index), name='index'),
+    path('<str:platform>/<str:board>/', cache_page(60 * 15)(views.index), name='index'),
+    path('<str:platform>/<str:board>/res/<int:thread_id>.html', cache_page(60 * 60 * 12)(views.thread), name='thread'),
 ]
