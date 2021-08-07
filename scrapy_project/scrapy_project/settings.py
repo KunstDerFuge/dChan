de28d9100f6d6ed8862383d116d19bdfa3224575
Robert Amour <robert@ferretech.io>
2021-08-07 03:21:19 -0700

    Correct Django project name in scrapy project settings

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath('.')))
 # Do not forget the change iCrawler part based on your project name
-os.environ['DJANGO_SETTINGS_MODULE'] = 'iCrawler.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'dChan.settings'
 
 # This is required only if Django Version > 1.8
 
