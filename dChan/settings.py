95a3e6df9bd94d9094a63dd792b0dcf8ad549e32
Robert Amour <robert@ferretech.io>
2021-08-06 22:50:33 -0700

    Make scrape jobs more frequent for testing

--- dChan/settings.py
+++ dChan/settings.py
@@ -108,11 +108,11 @@ CELERY_TIMEZONE = 'America/Los_Angeles'
 CELERY_BEAT_SCHEDULE = {
     'scrape-posts': {
         'task': 'posts.tasks.scrape_posts',
-        'schedule': 10 * 60,
+        'schedule': 2 * 60,
     },
     'create-scrape-jobs': {
         'task': 'posts.tasks.create_scrape_jobs',
-        'schedule': 15 * 60,
+        'schedule': 5 * 60,
     }
 }
 
