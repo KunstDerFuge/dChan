a6527a186269718ec2bb02ca35621eeb4398fae5
Robert Amour <robert@ferretech.io>
2021-08-07 03:19:03 -0700

    Correctly receive kwargs for start_urls in 8kun spider

--- scrapy_project/scrapy_project/spiders/8kun_spider.py
+++ scrapy_project/scrapy_project/spiders/8kun_spider.py
@@ -11,6 +11,7 @@ class EightKunSpider(scrapy.Spider):
     }
 
     def __init__(self, *args, **kwargs):
+        self.start_urls = kwargs.get('start_urls')
         super(EightKunSpider, self).__init__(*args, **kwargs)
 
     def parse(self, response, **kwargs):
