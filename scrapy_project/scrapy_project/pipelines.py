6d69cdbdccd4be22a53cac1f21deded2a835296a
Robert Amour <robert@ferretech.io>
2021-08-08 16:56:46 -0700

    Add print statements to 8kun_spider pipeline

--- scrapy_project/scrapy_project/pipelines.py
+++ scrapy_project/scrapy_project/pipelines.py
@@ -24,14 +24,21 @@ class ScrapyPostPipeline(object):
 
     def close_spider(self, spider):
         self.df['platform'] = '8kun'
+        print('Processing links...')
         self.df['links'] = self.df.apply(process_links, axis=1)
+        print('Parsing formatting...')
         self.df['body_text'] = self.df.body_text.apply(parse_formatting)
+        print('Committing to DB...')
         new_threads = utilities.commit_posts_from_df(self.df, self.platform)
+        print('Processing replies...')
         utilities.process_replies(new_threads)
 
+        print('Deleting finished jobs...')
         ScrapeJob.objects.filter(url__in=self.scraped_urls).delete()
         failed_urls = [url for url in self.start_urls if url not in self.scraped_urls]
         failed_jobs = ScrapeJob.objects.filter(url__in=failed_urls)
         for job in failed_jobs:
             job.error_count += 1
             job.save()
+
+        print('Done!')
