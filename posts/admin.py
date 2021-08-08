2c4c763bcff32087e3318fd8c8140bb6005d14f0
Robert Amour <robert@ferretech.io>
2021-08-07 18:55:53 -0700

    Register Board and Platform models in Admin interface

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from posts.models import Post, ScrapeJob
+from posts.models import Post, ScrapeJob, Board, Platform
 
 admin.site.register(Post)
 admin.site.register(ScrapeJob)
+admin.site.register(Platform)
+admin.site.register(Board)
