33cc13ad374b905f77b511d46af181472de67a6d
Robert Amour <robert@ferretech.io>
2021-08-08 16:53:37 -0700

    Update auto scrape job frequencies

--- dChan/settings.py
+++ dChan/settings.py
@@ -105,11 +105,11 @@ CELERY_TIMEZONE = 'America/Los_Angeles'
 CELERY_BEAT_SCHEDULE = {
     'scrape-posts': {
         'task': 'posts.tasks.scrape_posts',
-        'schedule': 2 * 60,
+        'schedule': 10 * 60,
     },
     'create-scrape-jobs': {
         'task': 'posts.tasks.create_scrape_jobs',
-        'schedule': 2 * 60,
+        'schedule': 5 * 60,
     }
 }
 
