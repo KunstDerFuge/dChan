0890320c30e6492e34e02da60a9f4c19f74a9822
Robert Amour <robert@ferretech.io>
2021-08-09 17:20:55 -0700

    Move all load_chan_data functions to utilities

--- posts/utilities.py
+++ posts/utilities.py
@@ -1,7 +1,9 @@
+import html
+import re
+
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-from posts.management.commands.load_chan_data import parse_formatting
 from posts.models import Post, Board
 
 
@@ -36,6 +38,20 @@ def process_replies(threads=None):
                 post.replies = sorted(all_replies[post.post_id], key=lambda x: x[0])
 
         Post.objects.bulk_update(posts, ['replies'])
+
+
+def process_and_commit_from_df(df, platform_obj):
+    print('Parsing formatting...')
+    df['body_html'] = df.body_text
+    if platform_obj.name == '8chan':
+        df['body_text'] = df.body_text.apply(parse_8chan_formatting)
+    else:
+        df['body_text'] = df.body_text.apply(parse_formatting)
+
+    print('Committing to DB...')
+    new_threads = commit_posts_from_df(df, platform_obj)
+    print('Processing replies...')
+    process_replies(new_threads)
 
 
 def commit_posts_from_df(df, platform_obj):
@@ -107,3 +123,130 @@ def parse_archive_is(row):
         print(row.header)
         print(e)
         raise e
+
+
+def parse_formatting(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Process green text
+    for result in soup.find_all(attrs={'class': 'quote'}):
+        result.insert(0, '> ')
+
+    # Process red text
+    for result in soup.find_all(attrs={'class': 'heading'}):
+        result.insert_before('==')
+        result.insert_after('==')
+
+    # Process bold text
+    for result in soup.find_all('strong'):
+        result.insert_before("'''")
+        result.insert_after("'''")
+
+    # Process italic text
+    for result in soup.find_all('em'):
+        if result.get_text() != '//':  # For some reason, the // in URLs is wrapped with <em />
+            result.insert_before("''")
+            result.insert_after("''")
+
+    # Process underlined text
+    for result in soup.find_all('u'):
+        result.insert_before("__")
+        result.insert_after("__")
+
+    # Process strikethrough text
+    for result in soup.find_all('s'):
+        result.insert_before("~~")
+        result.insert_after("~~")
+
+    # Process spoiler text
+    for result in soup.find_all(attrs={'class': 'spoiler'}):
+        result.insert_before("**")
+        result.insert_after("**")
+
+    final_text = '\n'.join([line.get_text() for line in soup.find_all(attrs={'class': 'body-line'})])
+    return final_text
+
+
+def process_links(row):
+    try:
+        links = dict()
+        if row['platform'] == '8chan':
+            board_index_links = re.findall(
+                r'\/([a-zA-Z0-9]+)\/index\.html\".{80,95}>(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/)',
+                row['body_text'])
+            matches = re.findall(
+                r'\/([a-zA-Z0-9]+)\/res\/([0-9]+)\.html%23([0-9]+)\".{80,95}>(&gt;&gt;[0-9]+|&gt;&gt;&gt;\/[a-zA-Z]+\/[0-9]+)',
+                row['body_text'])
+        else:
+            board_index_links = re.findall(
+                r'\"\/([a-zA-Z0-9]+)\/index\.html\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;/[a-zA-Z]+/)',
+                row['body_text'])
+            matches = re.findall(
+                r'\"\/([a-zA-Z0-9]+)\/res\/([0-9]+)\.html#q?([0-9]+)\">(&gt;&gt;[0-9]+|&gt;&gt;&gt;\/[a-zA-Z]+\/[0-9]+)',
+                row['body_text'])
+        matches.extend(board_index_links)
+        if len(matches) == 0:
+            return dict()
+        else:
+            for match in matches:
+                if len(match) == 2:
+                    # Board index link
+                    links[html.unescape(match[-1])] = f"/{row['platform']}/{match[0]}/"
+                else:
+                    links[html.unescape(match[-1])] = f"/{row['platform']}/{match[0]}/res/{match[1]}.html#{match[2]}"
+        return links
+    except Exception as e:
+        print(e)
+        return {}
+
+
+def split_list(lst, n):
+    from itertools import islice
+    lst = iter(lst)
+    result = iter(lambda: tuple(islice(lst, n)), ())
+    return list(result)
+
+
+def parse_8chan_formatting(html):
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Process green text
+    for result in soup.find_all(attrs={
+        'style': 'text-align:left;color:rgb(120, 153, '
+                 '34);direction:ltr;display:block;line-height:1.16em;font-size:13px;min-height:1.16em;margin: 0px; '}):
+        result.insert(0, '> ')
+
+    # Process red text
+    for result in soup.find_all(
+            attrs={'style': 'text-align:left;color:rgb(175, 10, 15);font-size:11pt;font-weight:bold;'}):
+        result.insert_before('==')
+        result.insert_after('==')
+
+    # Process bold text
+    for result in soup.find_all('strong'):
+        result.insert_before("'''")
+        result.insert_after("'''")
+
+    # Process italic text
+    for result in soup.find_all('em'):
+        if result.get_text() != '//':  # For some reason, the // in URLs is wrapped with <em />
+            result.insert_before("''")
+            result.insert_after("''")
+
+    # Process underlined text
+    for result in soup.find_all('u'):
+        result.insert_before("__")
+        result.insert_after("__")
+
+    # Process strikethrough text
+    for result in soup.find_all('s'):
+        result.insert_before("~~")
+        result.insert_after("~~")
+
+    # Process spoiler text
+    for result in soup.find_all(attrs={'class': 'spoiler'}):
+        result.insert_before("**")
+        result.insert_after("**")
+
+    final_text = '\n'.join([line.get_text() for line in soup.find_all('div')])
+    return final_text
