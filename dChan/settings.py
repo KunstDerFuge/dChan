012c2987c1c0bcf1acf7d98e8a717ca7a5d1adfe
Robert Amour <robert@ferretech.io>
2021-08-09 17:20:39 -0700

    Update task frequency

--- dChan/settings.py
+++ dChan/settings.py
@@ -105,11 +105,11 @@ CELERY_TIMEZONE = 'America/Los_Angeles'
 CELERY_BEAT_SCHEDULE = {
     'scrape-posts': {
         'task': 'posts.tasks.scrape_posts',
-        'schedule': 10 * 60,
+        'schedule': 15 * 60,
     },
     'create-scrape-jobs': {
         'task': 'posts.tasks.create_scrape_jobs',
-        'schedule': 5 * 60,
+        'schedule': 3 * 60,
     }
 }
 
