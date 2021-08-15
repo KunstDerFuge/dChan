0cfd664e86264a6a233ab46783bcdb5d9f3a931a
Robert Amour <robert@ferretech.io>
2021-08-14 19:38:16 -0700

    Don't revert to static image on mobile

--- visualizations/src/App.js
+++ visualizations/src/App.js
@@ -4,17 +4,13 @@ import {useMediaQuery} from '@material-ui/core'
 
 function App() {
   const isMobile = !useMediaQuery('(min-width:1280px)')
-  const smallScreen = !useMediaQuery('(min-width:650px)')
 
   return (
     <>
       {
-        smallScreen ?
-          <img style={{maxWidth: '95%'}} src={'/images/static.png'}/>
-          :
-          <div style={{height: isMobile ? 1000 : 500, width: isMobile ? 600 : 1250, padding: '10px'}}>
-            <Timeseries width={1200} height={400}/>
-          </div>
+        <div style={{height: isMobile ? 1000 : 500, width: '100%', padding: '10px'}}>
+          <Timeseries width={1200} height={400}/>
+        </div>
       }
     </>
   )
