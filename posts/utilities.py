b59aecafa397bf7db943a25164076d1a99ac6663
Robert Amour <robert@ferretech.io>
2021-08-20 02:08:47 -0700

    Correct row index syntax

--- posts/utilities.py
+++ posts/utilities.py
@@ -134,7 +134,7 @@ def commit_posts_from_df(df, platform_obj):
 
         threads.add(row['thread_no'])
         post = Post(platform=platform_obj, board=board, thread_id=row['thread_no'],
-                    post_id=row[int(float('post_no'))], author=row['name'], poster_hash=row['poster_id'],
+                    post_id=int(float(row['post_no'])), author=row['name'], poster_hash=row['poster_id'],
                     subject=row['subject'], body=row['body_text'], timestamp=row['timestamp'],
                     tripcode=row['tripcode'], is_op=(row['post_no'] == row['thread_no']),
                     links=row['links'], body_html=row['body_html'], replies=row['replies'])
