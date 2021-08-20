3abc8c9a442f38c071375ebfe5b42fdba2ae3095
Robert Amour <robert@ferretech.io>
2021-08-20 02:20:13 -0700

    Set jobs no longer in progress only after processing complete

--- scrapy_project/scrapy_project/pipelines.py
+++ scrapy_project/scrapy_project/pipelines.py
@@ -29,21 +29,24 @@ class ScrapyPostPipeline(object):
         return item
 
     def close_spider(self, spider):
-        if len(self.df) == 0:
-            return
-        self.df['platform'] = spider.platform
-        platform_obj = Platform.objects.get(name=spider.platform)
-        if spider.platform == '8chan':
-            self.df['timestamp'] = pd.to_datetime(self.df['timestamp'])
-            self.df['timestamp'] = self.df['timestamp'].dt.tz_localize(tz='UTC')  # 8chan timestamps are UTC
-        utilities.process_and_commit_from_df(self.df, platform_obj)
-        print('Deleting finished jobs...')
-        spider.jobs.update(in_progress=False)
-        spider.jobs.filter(url__in=self.scraped_urls).delete()
-        failed_urls = [url for url in self.start_urls if url not in self.scraped_urls]
-        failed_jobs = spider.jobs.filter(url__in=failed_urls)
-        for job in failed_jobs:
-            job.error_count += 1
-            job.save()
+        try:
+            if len(self.df) == 0:
+                return
+            self.df['platform'] = spider.platform
+            platform_obj = Platform.objects.get(name=spider.platform)
+            if spider.platform == '8chan':
+                self.df['timestamp'] = pd.to_datetime(self.df['timestamp'])
+                self.df['timestamp'] = self.df['timestamp'].dt.tz_localize(tz='UTC')  # 8chan timestamps are UTC
+            utilities.process_and_commit_from_df(self.df, platform_obj)
+            print('Deleting finished jobs...')
+            spider.jobs.filter(url__in=self.scraped_urls).delete()
+            failed_urls = [url for url in self.start_urls if url not in self.scraped_urls]
+            failed_jobs = spider.jobs.filter(url__in=failed_urls)
+            for job in failed_jobs:
+                job.error_count += 1
+                job.save()
 
-        print('Done!')
+            print('Done!')
+
+        finally:
+            spider.jobs.update(in_progress=False)
