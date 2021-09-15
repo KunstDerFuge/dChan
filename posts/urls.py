232be13f2401d68e6219ac1cf8da9a99e84f88d3
Robert Amour <robert@ferretech.io>
2021-09-15 13:25:06 -0700

    Update URLs to new timeseries frontend function

--- posts/urls.py
+++ posts/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from . import views
-from .views import AdvancedSearch, timeseries_from_keywords, FrontendAppView, search_results
+from .views import AdvancedSearch, timeseries_from_keywords, timeseries_frontend, search_results
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/advanced/', AdvancedSearch.as_view(), name='advanced_search'),
     path('search/', search_results, name='search_results'),
     path('data/', timeseries_from_keywords, name='timeseries'),
-    path('timeseries/', FrontendAppView.as_view(), name='timeseries_frontend'),
+    path('timeseries/', timeseries_frontend, name='timeseries_frontend'),
     path('drop/<int:drop_no>', views.drop, name='drop'),
     path('first/<str:phrase>', views.first_to_say, name='index'),
     path('<str:platform>/', cache_page(15 * 60)(views.index), name='index'),
