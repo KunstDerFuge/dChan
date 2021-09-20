e7076b192968ff871dc73da87810c70f64420d79
Robert Amour <robert@ferretech.io>
2021-09-20 15:59:15 -0700

    Don't cache blacklisted word definitions

--- chanscan/tasks.py
+++ chanscan/tasks.py
@@ -2,14 +2,17 @@ from celery import shared_task
 from django.core.cache import cache
 import requests
 
+from chanscan.models import Blacklist
+
 
 @shared_task
 def fetch_chanscan_definitions():
     chanscan_api_url = 'https://api.chanscan.com/all'
     response = requests.get(chanscan_api_url)
+    blacklist = [blacklist.word for blacklist in Blacklist.objects.all()]
     try:
         data = list(response.json())
-        data = [word for word in data if word['definition'] != '']
+        data = [word for word in data if word['definition'] != '' and word['word'] not in blacklist]
         if len(data) > 350:  # Simple sanity check so that we don't overwrite/delete valid definitions in case of errors
             cache.set('definitions', data, None)
 
