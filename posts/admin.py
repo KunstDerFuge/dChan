281f03e3eab6df07936ca20b17735fa379bcb286
Robert Amour <robert@ferretech.io>
2021-12-03 17:30:13 -0800

    Add Reddit models to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from posts.models import Post, ScrapeJob, Board, Platform
+from posts.models import Post, ScrapeJob, Board, Platform, RedditPost, Subreddit
 
 admin.site.register(Post)
 admin.site.register(ScrapeJob)
 admin.site.register(Platform)
 admin.site.register(Board)
+admin.site.register(RedditPost)
+admin.site.register(Subreddit)
