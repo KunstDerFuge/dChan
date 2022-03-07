ae5125545fb30934812b157cffd0b282a3b3a64a
Robert Amour <robert@ferretech.io>
2022-03-07 12:33:27 -0800

    Put Reddit posts on their own index!

--- posts/documents.py
+++ posts/documents.py
@@ -71,7 +71,7 @@ class RedditPostDocument(Document):
 
     class Index:
         # Name of the Elasticsearch index
-        name = 'posts'
+        name = 'reddit_posts'
         # See Elasticsearch Indices API reference for available settings
         settings = {'number_of_shards': 1,
                     'number_of_replicas': 0}
